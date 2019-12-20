ca998475ae46c79bf2f08418cf85e733759c54a0
jvdlem <jordivanderlem@gmail.com>
2019-12-20 20:26:47 +0100

    boy

---
+++ Assets/scripts/ShowAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    void Start()
+    {
+        animator.SetBool("Playanimation", true);
+    }
+}
