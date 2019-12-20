b0d2b520032d63959ee8c6d6894b49849f2eb4ea
jvdlem <jordivanderlem@gmail.com>
2019-12-20 19:04:05 +0100

    wolla

--- Assets/scripts/PlayerMovment.cs
+++ Assets/scripts/PlayerMovment.cs
@@ -10,9 +10,22 @@ public class PlayerMovment : MonoBehaviour
     [SerializeField] private float jumpStrength;
     [SerializeField] private bool onGround;
     [SerializeField] private bool canMove = true;
+    [SerializeField] private bool playSound = true;
     [SerializeField] private Animator playerAnimator;
     [SerializeField] private bool facingRight = true;
     [SerializeField] private AnimationClip dieAnimation;
+    [SerializeField] private AnimationClip shootAnimation;
+    [SerializeField] private AnimationClip walkAnimation;
+    [SerializeField] private AnimationClip jumpAnimation;
+    [SerializeField] private GameObject projectille;
+    [SerializeField] private Transform firepoint;
+    [SerializeField] private float yRotate;
+    [SerializeField] private AudioSource playerAudio;
+    [SerializeField] private AudioClip jumpAudio;
+    [SerializeField] private AudioClip shootAudio;
+    [SerializeField] private AudioClip walkAudio;
+    [SerializeField] private AudioClip landAudio;
+    [SerializeField] private AudioClip deathAudio;
     Vector2 directionalInput;
     Vector3 velocity;
     bool Donejumping;
@@ -33,24 +46,35 @@ public class PlayerMovment : MonoBehaviour
         Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         playerAnimator.SetFloat("Speed", Mathf.Abs(directionalInput.x));
         SetDirectionalInput(directionalInput);
-
+        if (yRotate >= 360)
+        {
+            yRotate = 0;
+        }
         if (Time.time - lastPlay >= jumpTime)
         {
             lastPlay = Time.time;
             playerAnimator.SetBool("Jump", false);
         }
-
         if (Input.GetKeyDown("space") && onGround == true && canMove == true)
         {
+
             jumpTime = playerAnimator.GetCurrentAnimatorStateInfo(0).length;
             playerAnimator.SetBool("Jump", true);
             playerRB.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
+            playerAudio.clip = jumpAudio;
+            playerAudio.Play();
             onGround = false;
+        }
+        if (directionalInput.x > 0 && playSound == true&& onGround == true|| directionalInput.x < 0 && playSound == true && onGround == true)
+        {
+            playSound = false;
+            StartCoroutine(walkcycle());
         }
         if (directionalInput.x > 0 && !facingRight || directionalInput.x < 0 && facingRight)
         {
             facingRight = !facingRight;
-                      
+            yRotate += 180;
+            firepoint.transform.rotation = Quaternion.Euler(new Vector3(0, yRotate, 0));
             Vector3 theScale = transform.localScale;
             
             theScale.x *= -1;
@@ -60,6 +84,11 @@ public class PlayerMovment : MonoBehaviour
         {
             playerAnimator.SetBool("Jump", false);
         }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine(Shoot());
+        }
+
 
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -71,6 +100,8 @@ public class PlayerMovment : MonoBehaviour
                 onGround = true;
                 playerAnimator.SetBool("Jump", false);
                 playerAnimator.SetTrigger("OnGround");
+                playerAudio.clip = landAudio;
+                playerAudio.Play();
             }
                 
         }
@@ -83,18 +114,35 @@ public class PlayerMovment : MonoBehaviour
             
         }
     }
-
-    private IEnumerator waitforAnimation()
-    {
-        yield return new WaitForSeconds(playerAnimator.GetCurrentAnimatorStateInfo(0).length);
-        canMove = true;
-    }
-
     private IEnumerator Die()
     {
-
+        playerAudio.clip = deathAudio;
+        playerAudio.Play();
         yield return new WaitForSeconds(dieAnimation.length);
         DestroyGameobject();
+    }
+    private IEnumerator Shoot()
+    {
+
+
+        playerAnimator.SetTrigger("shoot");
+        yield return new WaitForSeconds(shootAnimation.length/2);
+        playerAudio.clip = shootAudio;
+        playerAudio.Play();
+        Instantiate(projectille,firepoint.transform.position,firepoint.transform.rotation);
+        yield return new WaitForSeconds(shootAnimation.length / 2);
+        playerAnimator.ResetTrigger("shoot");
+
+
+
+    }
+    
+    private IEnumerator walkcycle()
+    {
+        playerAudio.clip = walkAudio;
+        playerAudio.Play();
+        yield return new WaitForSeconds(walkAnimation.length/2);
+        playSound = true;
     }
 
     private void DestroyGameobject()
@@ -106,4 +154,6 @@ public class PlayerMovment : MonoBehaviour
     {
         directionalInput = input;
     }
+
+
 }
---
+++ Assets/scripts/Projectille.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectille : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D projectilleRB;
+    [SerializeField] private Transform projectille;
+    [SerializeField] private int speed;
+    [SerializeField] private Animator bulletAnimator;
+    [SerializeField] private AnimationClip explodeAnimation;
+    [SerializeField] private AudioSource bulletAudio;
+    [SerializeField] private AudioClip explodeAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectilleRB.AddForce(transform.right * speed, ForceMode2D.Force);
+        StartCoroutine(DeathTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground"|| collision.gameObject.tag == "trap")
+        {
+            StartCoroutine(Die());
+
+        }
+        
+    }
+
+    private IEnumerator Die()
+    {
+        bulletAudio.clip = explodeAudio;
+        bulletAudio.Play();
+        bulletAnimator.SetTrigger("Explode");
+        yield return new WaitForSeconds(explodeAnimation.length);
+        Destroy(this.gameObject);
+    }
+
+    private IEnumerator DeathTimer()
+    {
+        yield return new WaitForSeconds(10);
+        StartCoroutine(Die());
+    }
+
+
+}
